99956862ff1492b9b06ead4822eda3e1dd63e81f
Cornellius Ngondo <devngondo@gmail.com>
2015-05-11 06:17:53 +0000

    Successfully created a DB and added information to it

--- app/src/main/java/db/example/ngondo/dbexample/CreateContact.java
+++ app/src/main/java/db/example/ngondo/dbexample/CreateContact.java
@@ -1,38 +1,46 @@
 package db.example.ngondo.dbexample;
 
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 
 public class CreateContact extends ActionBarActivity {
+    EditText contactName,contactMobile,contactEmail;
+    Context context = this;
+    UserDbHelper userDbHelper;
+    SQLiteDatabase sqLiteDatabase;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_contact);
+        contactName=(EditText)findViewById(R.id.add_name);
+        contactMobile=(EditText)findViewById(R.id.add_number);
+        contactEmail=(EditText)findViewById(R.id.add_email);
+    }
+    //Method will be invoked when the button is clicked
+    public void addContact(View v){
+        //Capture the information from the form
+        String name = contactName.getText().toString();
+        String mob = contactMobile.getText().toString();
+        String email = contactEmail.getText().toString();
+        //Now insert to the DB
+        userDbHelper = new UserDbHelper(context);
+        sqLiteDatabase = userDbHelper.getReadableDatabase();
+        //Adds a row of data to the DB
+        userDbHelper.addinformation(name,mob,email,sqLiteDatabase);
+        //Toast an output if this is successful
+        Toast.makeText(getBaseContext(),"Contact Created",Toast.LENGTH_LONG).show();
+        //After adding information, we now close the DB.
+        userDbHelper.close();
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_create_contact, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
 }
